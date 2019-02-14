144fbc042c752bae8ad429df287d512b182c29b8
Ivan Kockarević <i.kockarevic@vegaitsourcing.rs>
2019-02-14 15:27:28 +0100

    Implemented quick sort.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import {bubbleSort} from './algorithms/bubble_sort';
 import {mergeSort} from './algorithms/merge_sort';
+import {quickSort} from './algorithms/quick_sort';
 
-console.log(mergeSort([4, 1, 2, 3, 0, 5]));
+const array = [4, 1, 2, 3, 0, 5];
+
+console.log(bubbleSort(array));
+console.log(mergeSort(array));
+console.log(quickSort(array, 0, array.length - 1));
