f8ae6c2690151787e131df055016804a1781e948
Ivan Kockarevic <kockarevic.ivan@gmail.com>
2019-12-05 20:20:18 +0100

    Add binary search for arrays

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-import { bubbleSort } from './algorithms/arrays/sorting/bubble_sort';
-import { mergeSort } from './algorithms/arrays/sorting/merge_sort';
-import { quickSort } from './algorithms/arrays/sorting/quick_sort';
-import { insertionSort } from './algorithms/arrays/sorting/insertion_sort';
-import { maximumElement } from './algorithms/arrays/maximum_element';
-import { minimumElement } from './algorithms/arrays/minimum_element';
+// import { bubbleSort } from './algorithms/arrays/sorting/bubble_sort';
+// import { mergeSort } from './algorithms/arrays/sorting/merge_sort';
+// import { quickSort } from './algorithms/arrays/sorting/quick_sort';
+// import { insertionSort } from './algorithms/arrays/sorting/insertion_sort';
+// import { maximumElement } from './algorithms/arrays/maximum_element';
+// import { minimumElement } from './algorithms/arrays/minimum_element';
+import { binarySearch, binarySearchRecursive } from './algorithms/arrays/binary_search';
 
-import { belowMainDiagonal } from './algorithms/matrices/below_main_diagonal';
-import { aboveMainDiagonal } from './algorithms/matrices/above_main_diagonal';
-import { transpose } from './algorithms/matrices/transpose';
-import { flipHorizontal } from './algorithms/matrices/flip_horizontal';
-import { flipVertical } from './algorithms/matrices/flip_vertical';
-import Tree from './algorithms/trees/binary_tree';
+// import { belowMainDiagonal } from './algorithms/matrices/below_main_diagonal';
+// import { aboveMainDiagonal } from './algorithms/matrices/above_main_diagonal';
+// import { transpose } from './algorithms/matrices/transpose';
+// import { flipHorizontal } from './algorithms/matrices/flip_horizontal';
+// import { flipVertical } from './algorithms/matrices/flip_vertical';
+// import Tree from './algorithms/trees/binary_tree';
 
-const array = [4, 1, 2, 3, 0, -326, 678, 5, 0];
-const matrix = [[4, 7, 1],
-                [2, 6, 8],
-                [3, 9, 0]];
+// const array = [4, 1, 2, 3, 0, -326, 678, 5, 0];
+const sortedArray = [2, 4, 6, 8, 10, 12, 14, 16];
+// const matrix = [[4, 7, 1],
+//                 [2, 6, 8],
+//                 [3, 9, 0]];
 
 // console.log(bubbleSort(array));
 // console.log(mergeSort(array));
@@ -28,3 +30,5 @@ const matrix = [[4, 7, 1],
 // console.log(transpose(matrix));
 // console.log(flipHorizontal(matrix));
 // console.log(flipVertical(matrix));
+
+console.log(binarySearchRecursive(sortedArray, 6));
